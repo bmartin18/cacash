64472cb349fd493830e3100925f2c82c4e55a010
bmartin <brian.martin18@gmail.com>
2018-05-27 20:53:21 +0200

    fix float in transaction form

--- src/AppBundle/Form/TransactionType.php
+++ src/AppBundle/Form/TransactionType.php
@@ -8,7 +8,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -63,7 +63,7 @@ class TransactionType extends AbstractType
                 'label' => 'Pointage',
                 'required' => false,
             ])
-            ->add('amountDisplayable', IntegerType::class, [
+            ->add('amountDisplayable', NumberType::class, [
                 'label' => 'Montant',
                 'required' => true,
             ])
