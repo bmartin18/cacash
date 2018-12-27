f8135fe8c83383362ab779645ee92517b3c62f5c
bmartin <brian.martin18@gmail.com>
2018-12-27 15:34:28 +0100

    add simplethings/entity-audit-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
